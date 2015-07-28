43c6ec82a219ab8e7876c2f7817ca9737d2e2fbe
giannisdag <giannisdag@yahoo.gr>
2015-07-28 14:01:53 +0300

    Create fileinput.php
    add greek translations

---
+++ messages/el-GR/fileinput.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Message translations for \kartik\widgets\FileInput.
+ *
+ * It contains the localizable messages extracted from source code.
+ * You may modify this file by translating the extracted messages.
+ *
+ * Each array element represents the translation (value) of a message (key).
+ * If the value is empty, the message is considered as not translated.
+ * Messages that no longer need translation will have their translations
+ * enclosed between a pair of '@@' marks.
+ *
+ * Message string can be used with plural forms format. Check i18n section
+ * of the guide for details.
+ *
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'file preview and multiple file upload' => 'Προεπισκόπηση αρχείων και ανέβασμα πολλαπλών αρχείων',
+    'Note:' => 'Σημείωση:',
+    'multiple file upload' => 'Πολλαπλό ανέβασμα αρχείων',
+    'Your browser does not support {validation}. Try an alternative or more recent browser to access these features.' => 'Ο φυλλομετρητής (browser) σας δεν υποστηρίζει {validation}. Δοκιμάστε έναν άλλο φυλλομετρητή είτε αναβαθμίστε τον δικό σας ώστε να έχετε πρόσβαση σε αυτές τις δυνατότητες'
+];
